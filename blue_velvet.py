542babd45c0b9273018edca20b442afd3e543d04
qazav_szaszak <qazav3.0@gmail.com>
2013-09-25 15:11:15 -0300

    Update blue_velvet.py
    blue_velvet now generates a 1-track XML. TODO: check on how to read info from an audio file instead of using arbitrary parameters.

--- blue_velvet.py
+++ blue_velvet.py
@@ -20,6 +20,7 @@ system = bpy.context.user_preferences.system
 audioSampleRate = system.audio_sample_rate.split("_")[1]
 
 desktop = os.environ['HOMEPATH'] + os.sep + "Desktop"
+sourceAudiosFolder = desktop # change, must come from User choice
 
 xmlSections = [ "Config", "Metadata", "Sources", "Regions", "Locations", "Bundles",
              "Routes", "Playlists", "UnusedPlaylists", "RouteGroups", "Click",
@@ -30,25 +31,34 @@ xmlSections = [ "Config", "Metadata", "Sources", "Regions", "Locations", "Bundle
 atSession = {'version': 3001,
              'name': fileBasename,
              'sample-rate': audioSampleRate,
-             'id-counter': 4,
+             'id-counter': 2,
              'event-counter': 0
              }
                  
 atOption = {'name': "audio-search-path",\
-            'value': desktop
+            'value': sourceAudiosFolder
             }
 
-atLocation = {'id': [0, 1, 2],
-              'name': ["Loop", "Punch", "session"],
-              'start': [0, 0, 0],
-              'end': [24303908, 1, 24303908],
-              'flags': ["IsAutoLoop,IsHidden", "IsAutoPunch,IsHidden", "IsSessionRange"],
-              'locked': ["no", "no", "no"],
-              'position-lock-style': ["AudioTime", "AudioTime", "AudioTime"]
+#atLocation = {'id': [0, 1, 2],
+#              'name': ["Loop", "Punch", "session"], # Loop and Punch may not be necessary for base XML
+#              'start': [0, 0, 0], # Start is 0 for everyone - timeline start = 0
+#              'end': [24303908, 1, 24303908], # End is 1 for empty project (infinity) or end = endOfTimeline
+#              'flags': ["IsAutoLoop,IsHidden", "IsAutoPunch,IsHidden", "IsSessionRange"],
+#              'locked': ["no", "no", "no"],
+#              'position-lock-style': ["AudioTime", "AudioTime", "AudioTime"]
+#              }
+
+atLocation = {'id': 0,
+              'name': "session",
+              'start': 0, # Start is 0 - timeline start = 0
+              'end': 1, # End is 1 for empty project (infinity) or end = endOfTimeline
+              'flags': "IsSessionRange",
+              'locked': "no",
+              'position-lock-style': "AudioTime"
               }
 
 atIO = {'name': "click",
-        'id': 3,
+        'id': 1,
         'direction': "Output",
         'default-type': "audio",
         'user-latency': 0,
@@ -70,6 +80,143 @@ atMeter = {'start': "1|1|0",
            'movable': "no"
            }
 
+######## ------------------------------------
+
+atSource = {'name': "lala.wav", # audio file with extension
+            'type': "audio",
+            'flags': "",
+            'id': 41, # change id accordingly - this id influentiates atRegion in source-o and master-source-0
+            'channel': 0, # ???????
+            'origin': "lala.wav",} # audio file with extension - could be an absolute path
+
+atRegion = {'name': "lala", # audio file, without extension
+            'muted': 0, # 0 = not muted
+            'opaque': 1,
+            'locked': 0, # 0 = not locked
+            'video-locked': 0, # 0 = not locked? should be locked to video?
+            'automatic': 0,
+            'whole-file': 1, # 1 = whole file? I'm using whole file for this test
+            'import': 0,
+            'external': 1, # 1 = not imported to ardour's assets folder?
+            'sync-marked': 0,
+            'left-of-split': 0,
+            'right-of-split': 0,
+            'hidden': 0, # 0 = not hidden
+            'position-locked': 0, # 0 = not locked
+            'valid-transients': 0,
+            'start': 0, # position where audio starts in timeline? Mine starts at 0.
+            'length': 24303909, # duration of file in samples (seconds * samplerate)
+            'position': 0, # position where audio starts in timeline? Mine starts at 0.
+            'sync-position': 0,
+            'ancestral-start': 0, # can be audio-offset-start?
+            'ancestral-length': 0,
+            'stretch': 1,
+            'shift': 1,
+            'positional-lock-style': "AudioTime",
+            'layering-index': 0,
+            'envelope-active': 0, # envelope in Ardour is same as metastrip in Blender?
+            'default-fade-in': 0,
+            'default-fade-out': 0,
+            'fade-in-active': 1,
+            'fade-out-active': 1,
+            'scale-amplitude': 1,
+            'id': 43, # generic id
+            'type': "audio",
+            'first-edit': "nothing",
+            'source-0': 41, # this is same id as atSource's id
+            'master-source-0': 41, # this is same id as atSource's id
+            'channels': 1 # mono file
+            }
+
+atRoute = {'id': 52, # generic id, BUT will be the referenced by atPlaylist
+           'name': "lala", # audio file, without extension
+           'default-type': "audio",
+           'active': "yes",
+           'phase-invert': 0,
+           'denormal-protection': "no",
+           'meter-point': "MeterPostFader",
+           'meter-type': "MeterPeak",
+           'order-keys': "EditorSort=0:MixerSort=0",
+           'self-solo': "no",
+           'soloed-by-upstream': 0,
+           'soloed-by-downstream': 0,
+           'solo-isolated': "no",
+           'solo-safe': "no",
+           'monitoring': "",
+           'saved-meter-point': "MeterPostFader",
+           'mode': "Normal"
+           }
+
+atRouteIO = {'name': ["lala", "lala"], # audio file, without extension
+             'id': [69, 70], # generic id
+             'direction': ["Input", "Output"],
+             'default-type': ["audio", "audio"],
+             'user-latency': [0, 0]
+             }
+
+atRouteIOPort = {'type': ["audio", "audio"],
+                 'name': ["lala/audio_in 1", "lala/audio_out 1"] # audio file, without extension
+                 }
+
+atDiskstream = {'flags': "Recordable",
+                'playlist': "lala", # audio file, without extension
+                'name': "lala", # audio file, without extension
+                'id': 9, # generic id
+                'speed': "1.000000",
+                'capture-alignment': "Automatic",
+                'channels': 1 # mono file
+                }
+
+atPlaylist = {'id': 80, # generic id
+              'name': "lala", # audio file, without extension
+              'type': "audio",
+              'orig-track-id': 52, # generic id of atRoute!
+              'frozen': "no",
+              'combine-ops': 0
+              }
+
+# atPlaylistRegion seems to have the same attributes of atRegion, except for #name, #whole-file and #id (generic id)
+atPlaylistRegion = {'name': "lala.1",
+                    'muted': 0,
+                    'opaque': 1,
+                    'locked': 0,
+                    'video-locked': 0,
+                    'automatic': 0,
+                    'whole-file': 0,
+                    'import': 0,
+                    'external': 1,
+                    'sync-marked': 0,
+                    'left-of-split': 0,
+                    'right-of-split': 0,
+                    'hidden': 0,
+                    'position-locked': 0,
+                    'valid-transients': 0,
+                    'start': 0,
+                    'length': 24303909,
+                    'position': 0,
+                    'sync-position': 0,
+                    'ancestral-start': 0,
+                    'ancestral-length': 0,
+                    'stretch': 1,
+                    'shift': 1,
+                    'positional-lock-style': "AudioTime",
+                    'layering-index': 0,
+                    'envelope-active': 0,
+                    'default-fade-in': 0,
+                    'default-fade-out': 0,
+                    'fade-in-active': 1,
+                    'fade-out-active': 1,
+                    'scale-amplitude': 1,
+                    'id': 87, # generic id
+                    'type': "audio",
+                    'first-edit': "nothing",
+                    'source-0': 41, # this is same id as atSource's id
+                    'master-source-0': 41, # this is same id as atSource's id
+                    'channels': 1 # mono file
+                    }
+
+
+######## ------------------------------------
 
 def createSubElements(el, att):
     for key, value in att.items():
@@ -94,27 +241,78 @@ for section in xmlSections:
 
 # Create Option, IO, Tempo and Meter + Attributes
 Option = SubElement(Session[0], "Option") # Session > Config > Option
+Location = SubElement(Session[4], "Location")
 IO = SubElement(Session[10], "IO") # Session > Click > IO
 Tempo = SubElement(Session[12], "Tempo") # Session > TempoMap > Tempo
 Meter = SubElement(Session[12], "Meter")# Session > TempoMap > Meter
 
 createSubElements(Session, atSession)
 createSubElements(Option, atOption)
+createSubElements(Location, atLocation)
 createSubElements(IO, atIO)
 createSubElements(Tempo, atTempo)
 createSubElements(Meter, atMeter)
 
 
 # Create Location and Port + Attributes
-Location = ""
-for counter in range(valLength(atLocation)):
-    Location = SubElement(Session[4], "Location")  # Session > Location
-    createSubElementsMulti(Location, atLocation, counter)
+#Location = ""
+#for counter in range(valLength(atLocation)):
+#    Location = SubElement(Session[4], "Location")  # Session > Location
+#    createSubElementsMulti(Location, atLocation, counter)
 
 Port = ""
 for counter in range(valLength(atPort)):
     Port = SubElement(IO, "Port") # Session > Click > IO > Port
     createSubElementsMulti(Port, atPort, counter)
+
+
+
+
+
+
+
+
+######## ------------------------------------
+#xmlSections = [ "Config", "Metadata", "Sources", "Regions", "Locations", "Bundles",
+#             "Routes", "Playlists", "UnusedPlaylists", "RouteGroups", "Click",
+#             "Speakers", "TempoMap", "ControlProtocols", "Extra" ]
+
+Source = SubElement(Session[2], "Source")
+Region = SubElement(Session[3], "Region")
+Route = SubElement(Session[6], "Route")
+
+createSubElements(Source, atSource)
+createSubElements(Region, atRegion)
+createSubElements(Route, atRoute)
+
+#RouteIO = ""
+#for counter in range(valLength(atRouteIO)):
+#    RouteIO = SubElement(Route, "IO")
+#    createSubElementsMulti(RouteIO, atRouteIO, counter)
+    
+#RouteIOPort = ""
+#for counter in range(valLength(atRouteIOPort)):
+#    RouteIOPort = SubElement(RouteIO, "Port")
+#    createSubElementsMulti(RouteIOPort, atRouteIOPort, counter)
+
+RouteIO = ""
+RouteIOPort = ""
+for counter in range(valLength(atRouteIO)):
+    RouteIO = SubElement(Route, "IO")
+    RouteIOPort = SubElement(RouteIO, "Port")
+    createSubElementsMulti(RouteIO, atRouteIO, counter)
+    createSubElementsMulti(RouteIOPort, atRouteIOPort, counter)
+
+
+Diskstream = SubElement(Route, "Diskstream")
+Playlist = SubElement(Session[7], "Playlist")
+PlaylistRegion = SubElement(Playlist, "Region")
+
+createSubElements(Diskstream, atDiskstream)
+createSubElements(Playlist, atPlaylist)
+createSubElements(PlaylistRegion, atPlaylistRegion)
+
+####### ------------------------------------
 
 
 
@@ -156,7 +354,7 @@ from xml.dom.minidom import parseString
 def identXML(element):
     return parseString(etree.tostring(element, "UTF-8")).toprettyxml(indent="  ")
 
-outXML = desktop + os.sep + fileBasename + ".xml"
+outXML = desktop + os.sep + fileBasename + ".xml" # should be similar to #sourceAudiosFolder, get from UI
 with open(outXML, 'w') as xmlFile:
     xmlFile.write(identXML(Session))
 
