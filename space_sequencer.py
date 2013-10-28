16756e06223efbd7acbd42d3817d409f39ee403b
qazav_szaszak <qazav3.0@gmail.com>
2013-10-28 17:51:41 -0200

    Inverted order of selected strip length

--- space_sequencer.py
+++ space_sequencer.py
@@ -165,9 +165,9 @@ class SEQUENCER_HT_header(Header):
             if context.sequences:
                 strip = act_strip(context)
                 frameStart = strip.frame_start + strip.frame_offset_start
-                col.label(" %s | %s (%s) " % (smpte(frameStart),
-                                              smpte(strip.frame_final_end),
-                                              smpte(strip.frame_final_duration)))
+                col.label(" (%s) %s | %s " % (smpte(strip.frame_final_duration),
+                                              smpte(frameStart),
+                                              smpte(strip.frame_final_end)))
 
             layout.prop(scene, "sync_mode", text="")
 
