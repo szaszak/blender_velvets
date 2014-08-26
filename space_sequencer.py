631df557ba5857ddb9cde261035d6a79b01fe997
qazav_szaszak <qazav3.0@gmail.com>
2014-08-26 13:45:01 -0300

    Updated space_sequencer to Blender 2.71

--- space_sequencer.py
+++ space_sequencer.py
@@ -17,8 +17,8 @@
 # ##### END GPL LICENSE BLOCK #####
 
 
-# velvet [ mod version ] - 20140424
-# To be used with Blender version 2.7 and onwards
+# velvet [ mod version ] - 20140826
+# To be used with Blender version 2.71 and onwards
 # mod by qazav_szaszak
 
 
@@ -242,7 +242,16 @@ class SEQUENCER_MT_view(Menu):
         if st.view_type in {'PREVIEW', 'SEQUENCER_PREVIEW'}:
             layout.operator_context = 'INVOKE_REGION_PREVIEW'
             layout.operator("sequencer.view_all_preview", text="Fit preview in window")
-            layout.operator("sequencer.view_zoom_ratio", text="Show preview 1:1").ratio = 1.0
+
+            layout.separator()
+
+            ratios = ((1, 8), (1, 4), (1, 2), (1, 1), (2, 1), (4, 1), (8, 1))
+
+            for a, b in ratios:
+                layout.operator("sequencer.view_zoom_ratio", text=iface_("Zoom %d:%d") % (a, b), translate=False).ratio = a / b
+
+            layout.separator()
+
             layout.operator_context = 'INVOKE_DEFAULT'
 
             # # XXX, invokes in the header view
@@ -568,7 +577,7 @@ class SEQUENCER_PT_effect(SequencerButtonsPanel, Panel):
     def draw(self, context):
         layout = self.layout
 
-        sequencer = context.scene.sequence_editor
+
         strip = act_strip(context)
 
         if strip.input_count > 0:
